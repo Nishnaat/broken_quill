933d2d3dd679c443078ca5b8164f82f1614e831c
Nishnaat Nagar <nishnaatnagar@gmail.com>
2025-06-11 15:47:09 +0530

    Fixed Home Page

--- broken_quill_frontend/src/pages/Home.js
+++ broken_quill_frontend/src/pages/Home.js
@@ -7,7 +7,7 @@ function Home({ darkMode }) {
 	const [featured, setFeatured] = useState(null);
 
 	useEffect(() => {
-		axios.get('${process.env.REACT_APP_API_URL}/api/posts')
+		axios.get(`${process.env.REACT_APP_API_URL}/api/posts`)
 			.then(res => {
 				setPosts(res.data);
 
